b9337c779668ccca5c2796e54fd7b43d339b75fb
Joaquin Lorca Nieto <joaquin.lorca.nieto@ces-vegamedia.es>
2024-12-19 09:56:10 +0100

    Commit 8.4

--- tienda/laravel/routes/web.php
+++ tienda/laravel/routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
+use App\Models\Product;
 
 Route::get('products', function () {
     return view('products.index');
-});
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return view('products.create');
+})->name('products.create');
+
+Route::post('products', function (Request $request) {
+    $newProduct = new Product;
+    $newProduct -> description = $request->input('description');
+    $newProduct -> price = $request->input('price');
+    $newProduct -> save();
+    return redirect()->route('products.index');
+})->name('products.store');
